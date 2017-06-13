7db9d9299b2f7d144bd2832d6fb0548498176e72
tobiasbeck <tob.beck@icloud.com>
2017-06-13 15:55:08 +0200

    added basic form

--- app/controllers/FormController.java
+++ app/controllers/FormController.java
@@ -1,7 +1,24 @@
 package controllers;
 
+import models.*;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.Result;
+
+import javax.inject.Inject;
+
+import static play.mvc.Results.*;
+
 /**
  * Created by tobibeck on 13.06.17.
  */
 public class FormController {
+
+    @Inject
+    public FormFactory formFactory;
+
+    public Result addUser(){
+        Form<User> userForm = formFactory.form(User.class);
+        return ok(views.html.nutzerErstellen.render(userForm));
+    }
 }
--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import play.mvc.Result;
+
+import static play.mvc.Results.ok;
+
 /**
  * Created by tobibeck on 13.06.17.
  */
 public class UserController {
+
+    public Result create(){
+        return ok("HELLO");
+    }
 }
